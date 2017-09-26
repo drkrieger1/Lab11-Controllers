b38de923be8cc431fa05fb3eefd5cce45e55e794
drkrieger <eplyushko@gmail.com>
2017-09-26 16:13:28 -0700

    almost done

---
+++ lab11_erik/lab11_erik/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab11_erik.Controllers
+{
+    public class ErrorController
+    {   
+        [Route("Error")]
+        public string Error()
+        {
+            return "Error! something is broken";
+        }
+    }
+}
---
+++ lab11_erik/lab11_erik/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab11_erik.Controllers
+{   
+    public class StudentController
+    {   
+        [Route("Student/Print")]
+        public string Print()
+        {
+            return "Printing all students...";
+        }
+        [Route("Student/Find")]
+        public string Find(string name, int age)
+        {
+            return $"looking for: {name} age:{age}";
+        }
+    }
+}
